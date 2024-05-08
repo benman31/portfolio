e3284e0230fed3f0954bd0788bf96731365189ab
benman31 <enman.ben@gmail.com>
2024-05-08 20:12:26 -0300

    Finished rough draft of animated logo

--- src/components/AnimatedLogo.tsx
+++ src/components/AnimatedLogo.tsx
@@ -1,16 +1,37 @@
-import Icon from './Icon';
+import { useState } from 'react';
 import '../styles/AnimatedLogo.less';
 
-//TODO make the actual animated logo
-// test
+//type Vec2D = {x: number, y: number}
+
+const sides = [
+  'show-front',
+  'show-left',
+  'show-back',
+  'show-right',
+]
+
 function AnimatedLogo() {
+  const [current, setCurrent] = useState(0)
+
+  const handleClick = () => {
+    setCurrent((current + 1)%4)
+  }
+
   return (
-    <>
-      <div id="animated-logo">
-        {/* placeholder */}
-        <Icon style='cube' color='green-highlight'></Icon>
-      </div>
-    </>
+    <div id="animated-logo">
+      <div className="logo-scene">
+        <div className={`parent-transform ${sides[current]}`} onClick={handleClick}>
+          <div className={"cube"}>
+            <div className="cube-face front"></div>
+            <div className="cube-face back"></div>
+            <div className="cube-face right"></div>
+            <div className="cube-face left"></div>
+            <div className="cube-face top"></div>
+            <div className="cube-face bottom"></div>
+          </div>
+        </div>
+    </div>
+  </div>
   )
 }
 
